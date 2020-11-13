a441890515c353407247944b28e7a0faaa59b56b
James Robinson <james.robinson@raspberrypi.org>
2020-11-13 12:06:10 +0000

    Added faces dict

--- en/solutions/robot_face.py
+++ en/solutions/robot_face.py
@@ -5,7 +5,7 @@ mouth_r = bh.port.C.motor
 mouth_l = bh.port.D.motor
 eb = bh.port.A.motor
 
-def move_eb (pos,speed):
+def move_eb (pos,speed=100):
   if pos > 170:
     pos = 170
   elif pos < -170:
@@ -13,7 +13,6 @@ def move_eb (pos,speed):
 
   current_pos = eb.get()[2]
   eb.run_to_position(pos,speed)
-
 
 def move_mouth (side, pos, speed=100):
   if pos > 45:
@@ -27,15 +26,20 @@ def move_mouth (side, pos, speed=100):
   elif side == "r":
     mouth_r.run_to_position(pos,speed)
 
-move_mouth("l",45)
-input()
-move_mouth("l",-45)
-input()
-move_mouth("l",450)
-input()
-move_mouth("r",45)
-input()
-move_mouth("r",-45,10)
-input()
-move_mouth("R",45)
-input()
+def change_eye(side,img):
+  pass
+
+
+faces = {
+  "happy":{"mouth_r":-45,"mouth_l":-45,"right_eye":"happy.png","left_eye":"happy.png","eyebrows":0}
+}
+
+def set_face (face):
+  change_eye("l",face["eye_l"])
+  change_eye("r",face["eye_r"])
+  move_mouth("l",face["mouth_l"])
+  move_mouth("r",face["mouth_r"])
+  move_eb("0")
+
+
+set_face(faces[happy])
