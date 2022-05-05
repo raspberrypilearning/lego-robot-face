04dcb80f4810e2782824ef075ea2df0eaf3763e6
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-05-05 11:36:48 +0100

    New translations robot_face.py (Japanese)

--- ja-JP/solutions/robot_face.py
+++ ja-JP/solutions/robot_face.py
@@ -5,28 +5,28 @@ from PIL import Image
 from classifier import Classifier
 from time import sleep
 
-## Set up the motors
+## モーターのセットアップ
 mouth_r = Motor('A')
 mouth_l = Motor('B')
 eyebrows = Motor('C')
 
-## Move the motors to 0 position
+## モーターを0の位置に移動
 mouth_r.run_to_position(0)
 mouth_l.run_to_position(0)
 eyebrows.run_to_position(0)
 
-## Set up the eyes
+## 目の設定
 i2c = board.I2C()
 left_eye = Matrix8x8(i2c, address=0x70)
 right_eye = Matrix8x8(i2c, address=0x71)
 
-## Link names of expressions to images in the Resources directory for the eyes to display
+## 表情の名前と、目を表示するためのリソースディレクトリ内の画像を関連付け
 neutral = Image.open("neutral.png").rotate(90)
 wide = Image.open("wide.png").rotate(90)
 angry = Image.open("angry.png").rotate(90)
 look_down = Image.open("look_down.png").rotate(90)
 
-## Link names of expressions to motor movement and to eye display in a dictionary
+## 表情の名前と、モーターの動作・辞書内の目の表示を関連付け
 faces = {
     "neutral":{"mouth":0, "right_eye":neutral, "left_eye":neutral, "eyebrows":0},
     "happy":{"mouth":45, "right_eye":wide, "left_eye":wide, "eyebrows":-150},
@@ -34,20 +34,20 @@ faces = {
     "sad":{"mouth":-45, "right_eye":look_down, "left_eye":look_down, "eyebrows":-40}
     }
 
-## Use the classifier.py to recognise different images (file is in resources directory)
+## 様々な画像を認識させるために classifier.py を使用する (ファイルはリソースディレクトリにあります)
 seen_items = Classifier(label_file="labels.txt",model_file="model.tflite",threshold=0.5)
 
-## Set reactions for different objects that are recognised
+## 様々なオブジェクトが認識された時のリアクションの設定
 reactions = {"broccoli":"neutral", "teapot":"happy", "Indian cobra":"angry", "hotdog":"happy"}
 
 def move_mouth (position):
-    '''Move the mouth to value of position parameter'''
+    '''口を position パラメータの位置に移動'''
     mouth_l.run_to_position(position * -1, blocking=False)
     mouth_r.run_to_position(position, blocking=False)
     
     
 def move_eyebrows (position):
-    '''Move the eyebrows to value of position parameter'''
+    '''眉毛を position パラメータの位置に移動'''
     current_position = eyebrows.get_aposition()
     if position < current_position:
         rotation = 'anticlockwise'
@@ -57,18 +57,18 @@ def move_eyebrows (position):
 
 
 def change_eyes(left, right):
-    '''display the PIL objects on the left and right eye'''
+    '''PILオブジェクトを左目と右目に表示する'''
     left_eye.image(left)
     right_eye.image(right)
 
 
 def set_face (face):
-    '''call all functions that change the expression, according to the face from the faces dictionary'''
+    '''顔の辞書内の顔に合わせて、表情を変更する関数を全部呼び出す'''
     change_eyes(face["right_eye"],face["left_eye"])
     move_mouth(face["mouth"])
     move_eyebrows(face["eyebrows"])
 
-## Loop forever and check the list of seen items and set the correct face if the object has been seen
+## 無限ループで見られたアイテムを確認して、物が見られた時に正しい顔を設定する
 while True:
     sleep(1)
     if seen_items.item != seen_items.last_item:
